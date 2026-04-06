ff9d19d5248e0e1daec16c8a39d794b0df72a081
Joud Aljabri <s202277140@kfupm.edu.sa>
2026-04-06 13:00:59 +0300

    ToDo4

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,5 +1,6 @@
 // TODO 1: Import and Create express app instance
 import express from "express";
+
 const app = express();
 
 // TODO 1: Define server port
@@ -7,7 +8,8 @@ const PORT = 3000;
 
 
 // TODO 4: Import and Apply CORS middleware
-
+import cors from "cors";
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
 
